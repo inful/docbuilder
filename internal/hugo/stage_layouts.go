1a15113002369a5232778a2bcf541af81f1c3ecd
Jone Marius Vignes <jvig@ihelse.net>
2025-12-16 11:06:09 +0100

    fix: copy taxonomy layouts

--- internal/hugo/stage_layouts.go
+++ internal/hugo/stage_layouts.go
@@ -9,6 +9,10 @@ func stageLayouts(_ context.Context, bs *BuildState) error {
 	if err := bs.Generator.copyTransitionAssets(); err != nil {
 		return err
 	}
+	// Copy taxonomy layouts if taxonomies are configured
+	if err := bs.Generator.copyTaxonomyLayouts(); err != nil {
+		return err
+	}
 	if bs.Generator.config != nil && bs.Generator.config.Hugo.Theme != "" {
 		var _ = bs.Generator.config.Hugo.Theme
 		return nil
